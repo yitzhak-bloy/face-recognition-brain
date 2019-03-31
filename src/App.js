aaf7590bbcbd5d323acb97620851fc84ca1414c1
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-03-31 15:54:12 +0300

    Building recognition of the image 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '02e7bcbd06b942b58e8363e3574a64d8'
+ });
 
 const particlesOptions = {
   particles: {
@@ -30,18 +36,36 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        "https://samples.clarifai.com/metro-north.jpg")
+        .then(
+        function(response) {
+            console.log(response)
+        },
+        function(err) {  
+          // there was an error}
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
         <Particles className='particles'
           params= {particlesOptions}
         /> 
-
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
-        {/* <FaceRecognition />  */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
+        <FaceRecognition /> 
         {/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
       </div>
     );
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FaceRecognition = () => {
+    return (
+        <div className='center'>
+            <img alt="sdf" src={'https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/m(01-32)_gr.jpg'} />
+        </div>
+    );
+}
+
+export default FaceRecognition;
