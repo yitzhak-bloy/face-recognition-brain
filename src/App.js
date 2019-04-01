01595ed85282c9db8fb6b1079edc35748831e4e3
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-04-01 10:26:28 +0300

    Building recognition of the image 4

--- src/App.js
+++ src/App.js
@@ -29,8 +29,17 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
@@ -43,14 +52,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-        .then(
-        function(response) {
-            console.log(response.outputs[0].data.regions[0].region_info.bounding_box )
-        },
-        function(err) {  
-          // there was an error}
-        }
-      );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err))
   }
 
   render() {
